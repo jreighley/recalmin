dde8501f0626f26fbed3da3a6dc595d67b7417fa
Timothy Pratley <timothypratley@gmail.com>
2017-01-24 20:00:35 -0800

    box selector

--- src/recalmin/core.cljs
+++ src/recalmin/core.cljs
@@ -1,30 +1,95 @@
 (ns recalmin.core
   (:require
     [reagent.core :as reagent :refer [atom]]
-    [recalcitrant.core :refer [error-boundary]]))
+    [recalcitrant.core :refer [error-boundary]]
+    [clojure.string :as string]))
 
 (enable-console-print!)
 
-(println "This text is printed from src/recalmin/core.cljs. Go ahead and edit it and see reloading in action.")
+(defonce app-state (reagent/atom []))
 
-;; define your app data so that it doesn't get over-written on reload
+(defn xy [e]
+  (let [rect (.getBoundingClientRect (.-target e))]
+    [(- (.-clientX e) (.-left rect))
+     (- (.-clientY e) (.-top rect))]))
 
-(defonce app-state (atom {:text "Hello world!"}))
+(defn svg [attrs boxes img]
+  [:svg
+   (merge-with merge attrs
+               {:width 800 :height 800
+                :stroke "black"
+                :fill "none"
+                :style {:border "1px solid"
+                        :cursor "crosshair"}})
+   (when @img
+     [:image {:xlink-href @img
+              :style {:pointer-events "none"}
+              :width "100%"
+              :height "100%"
+              :opacity 0.3}])
+   (for [[[x1 y1] [x2 y2]] @boxes]
+     [:path {:d (str "M" x1 " " y1 " L " (string/join " " [x1 y2 x2 y2 x2 y1 x1 y1]))}])])
 
-(defn foo []
-  ;;(throw "BAD")
-  [:h1 (:text @app-state)])
+(defn mouse-handlers [boxes]
+  (let [pen-down? (reagent/atom false)
+        start-xy (reagent/atom nil)
+        start-box
+        (fn [e]
+          (when (not= (.-buttons e) 0)
+            (reset! pen-down? true)
+            (let [coords (xy e)]
+              (reset! start-xy coords))))
+        end-box
+        (fn [e]
+          (when @pen-down?
+            (swap! boxes conj [@start-xy (xy e)])
+            (reset! pen-down? false)))]
+  {:on-mouse-down start-box
+   :on-mouse-over start-box
+   :on-mouse-up end-box
+   :on-mouse-out end-box}))
 
-(defn bar []
-  ;;(throw "BAD")
-  [:h1 "sup"])
+(defn table [boxes]
+  [:table
+   [:thead
+    [:tr
+     [:td "x1"]
+     [:td "y1"]
+     [:td "x2"]
+     [:td "y2"]]]
+   [:tbody
+    (for [[[x1 y1] [x2 y2]] @boxes]
+      [:tr
+       [:td x1]
+       [:td y1]
+       [:td x2]
+       [:td y2]])]])
 
-(defn root []
+(defonce img (reagent/atom nil))
+
+(defn root [boxes]
   [:div
-   [bar]
-   [error-boundary [foo]]])
+   [:div
+    [:label
+     [:input
+      {:type "file"
+       :accept "image/*"
+       :style {:display "none"}
+       :on-change
+       (fn image-selected [e]
+         (let [r (js/FileReader.)]
+           (set! (.-onload r)
+                 (fn [e]
+                   (reset! img (.. e -target -result))))
+           (.readAsDataURL r (aget (.. e -target -files) 0))))}]
+     [:span.mdl-button.mdl-button--icon
+      {:title "Background"}
+      [:i.material-icons "\uE3F4"]
+      "Add backgound"]]]
+   [svg (mouse-handlers boxes) boxes img]
+   [table boxes]])
 
-(reagent/render-component [(fn [] [error-boundary [root]])]
+(reagent/render-component [(fn [] [error-boundary [root app-state]])]
                           (. js/document (getElementById "app")))
 
 (defn on-js-reload []
